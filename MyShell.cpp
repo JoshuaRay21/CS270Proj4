4ff1374145e4d21957a1c02480fc566af3777f64
Luke Miles <rashreportloaf@gmail.com>
2018-04-03 23:12:04 -0400

    added non-working start of tokenizing REPL

--- MyShell.cpp
+++ MyShell.cpp
@@ -1,1 +1,29 @@
-//This is just test if push works
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
+void printTokens(std::istringstream iss) {
+    std::vector<std::string> v;
+    std::istringstream s;
+
+    while (iss >> std::quoted(s)) {
+        v.push_back(s);
+    }
+
+    for(auto& str: v)
+        std::cout << str << std::endl;
+}
+
+int main() {
+    std::string command;
+    while (true) {
+        std::cout << "Enter command: ";
+        std::getline(std::cin, command);
+        std::cout << "Command: \"" << command << "\"!\n";
+        printTokens(command);
+    }
+    return 0;
+}
+
