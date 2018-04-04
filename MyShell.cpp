2261997913e8aa4af5ec5fe07bc6bc05a5347caa
Unknown <jlra238@uky.edu>
2018-04-04 13:59:45 -0400

    just some touch ups, and starting of done

--- MyShell.cpp
+++ MyShell.cpp
@@ -18,11 +18,15 @@ void doRun(vector<string> tokens);
 void doFly(vector<string> tokens);
 void doTovar(vector<string> tokens);
 bool errorchecker(vector<string> tokens, int wantedArguements);
+void showprocs(vector<string> tokens);
+void done(vector<string> tokens);
+
 
 string ShowTokens = "0";
 map<string, string> variables; //1st string is the name, 2nd is value
 string prompt = "msh > ";
 vector<string> procs; //vec of processes running in background
+int exiting = -1; //this int is changed if done is called, if this is non neg, msh exits
 
 vector<string> createTokens(string str) {
 	istringstream iss(str);
@@ -43,19 +47,38 @@ void execCommand(vector<string> tokens) {
 	else if (command == "setprompt") {
 		changePrompt(tokens);
 	}
-	else if (command = "setdir") {
+	else if (command == "setdir") {
 		//JRAY DOESNT KNOW HOW TO DEAL WITH DIRECTORY
 	}
-	else if (command = "showprocs") {
+	else if (command == "showprocs") {
 		showprocs(tokens);
 	}
-	else if (command = "#") {
-
+	else if (command == "#") {
+		//nothing belongs in here, because this means the entire line is a comment
+	}
+	else if (command == "done") {
+		done(tokens);
 	}
 	//keep creating else if's for rest of commands
 }
+void done(vector<string> tokens) {
+	//if no value parameter, change exiting to 0; 
+	if (tokens.size() == 1) {
+		if(errorchecker(tokens, 1))
+			return;
+		exiting = 0;
+
+	}
+	else {
+		if(errorchecker(tokens, 2))
+			return;
+		string value = tokens[1];
+
+	}
+}
 void showprocs(vector<string> tokens) {
-	errorchecker(tokens, 1);
+	if(errorchecker(tokens, 1))
+		return;
 	for (auto& str : procs)
 		cout << str << endl;
 }
@@ -72,13 +95,17 @@ void setvar(vector<string> tokens) {
 	string variable = tokens[1];
 	
 	//if the first character in the variable is not a letter
-	if (!isalpha(variable[0])) {
+	if (isalpha(variable[0])) {
+	}
+	else{
 		fprintf(stderr,"Variable names must start with a letter.");
 		return;
 	}
 	//if the variable consists of anything other than letters/numbers
 	for (int i = 1; i < variable.length(); i++) {
-		if (!isalnum(variable[i])) {
+		if (isalnum(variable[i])) {
+		}
+		else{
 			fprintf(stderr,"Variable names must only consist of letters and numbers.");
 			return;
 		}
