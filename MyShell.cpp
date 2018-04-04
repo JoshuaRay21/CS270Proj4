cbab0eb4c98203d587dbbfc5da74176281026b7d
Unknown <jlra238@uky.edu>
2018-04-04 01:56:52 -0400

    Formatted

--- MyShell.cpp
+++ MyShell.cpp
@@ -20,24 +20,25 @@ string prompt = "msh > ";
 //The 2 above vectors should be thought of as parallel, 
 
 vector<string> createTokens(string str) {
-    istringstream iss(str);
-    vector<string> tokens;
-    string s;
+	istringstream iss(str);
+	vector<string> tokens;
+	string s;
 
-    while (iss >> quoted(s)) {
-        tokens.push_back(s);
-    }
+	while (iss >> quoted(s)) {
+		tokens.push_back(s);
+	}
 
-    return tokens;
+	return tokens;
 }
 void execCommand(vector<string> tokens) {
-    string command = tokens[0];
-    if (command == "setvar") {
-        setvar(tokens);
-    } else if (command == "setprompt") {
-        changePrompt(tokens);
-    }
-    //keep creating else if's for rest of commands
+	string command = tokens[0];
+	if (command == "setvar") {
+		setvar(tokens);
+	}
+	else if (command == "setprompt") {
+		changePrompt(tokens);
+	}
+	//keep creating else if's for rest of commands
 }
 void setvar(vector<string> tokens) {
 	if (tokens.size() > 3) {
@@ -45,51 +46,51 @@ void setvar(vector<string> tokens) {
 			return;
 			perror("Too many arguments for setvar");
 		}
-}
-	
-	variables.insert(pair<string, string>(variable, value));
-		perror("Too few arguments for setvar");
-		return;
-	}
-	if (value == "#") {
-	}
-	string value = tokens[2];
-	for (int i = 1; i < variable.length(); i++) {
-		}
-			perror("Variable names must only consist of letters and numbers.");
-		if (!isalnum(variable[i])) {
-	}
-		return;
-		perror("Variable names must start with a letter.");
-	if (!isalpha(variable[0])) {
-	}
-		return;
-		perror("Too few arguments for setvar");
-	if (variable == "#") {
-	string variable = tokens[1];
-		return;
-	}
-		perror("Too few arguments for setvar");
-	if (tokens.size() < 3) {
 	}
 
+	variables.insert(pair<string, string>(variable, value));
+	perror("Too few arguments for setvar");
+	return;
+}
+if (value == "#") {
+}
+string value = tokens[2];
+for (int i = 1; i < variable.length(); i++) {
+}
+perror("Variable names must only consist of letters and numbers.");
+if (!isalnum(variable[i])) {
+}
+return;
+perror("Variable names must start with a letter.");
+if (!isalpha(variable[0])) {
+}
+return;
+perror("Too few arguments for setvar");
+if (variable == "#") {
+	string variable = tokens[1];
+	return;
+}
+perror("Too few arguments for setvar");
+if (tokens.size() < 3) {
+}
+
 void changePrompt(vector<string> tokens) {
-    prompt = tokens[1];
+	prompt = tokens[1];
 }
 
 int main() {
-    string command;
-    vector<string> parameters;
-    string envp = "PATH";
-    while (true) {
-        cout << prompt;
-        getline(cin, command);
-        cout << "Command: \"" << command << "\"!\n";
-        vector<string> tokens = createTokens(command);
-        if (ShowTokens)
-            for(auto& str: tokens)
-                cout << str << endl;
-    }
-    return 0;
+	string command;
+	vector<string> parameters;
+	string envp = "PATH";
+	while (true) {
+		cout << prompt;
+		getline(cin, command);
+		cout << "Command: \"" << command << "\"!\n";
+		vector<string> tokens = createTokens(command);
+		if (ShowTokens)
+			for (auto& str : tokens)
+				cout << str << endl;
+	}
+	return 0;
 }
 
