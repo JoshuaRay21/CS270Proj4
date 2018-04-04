9700029461800059acbed755c984400c223566c7
Luke Miles <rashreportloaf@gmail.com>
2018-04-04 01:29:03 -0400

    added start of 'changePrompt'

--- MyShell.cpp
+++ MyShell.cpp
@@ -5,12 +5,21 @@
 #include <iomanip>
 
 #define PARAMETERSMAX 20
-bool ShowTokens = false;
-vector<string> variableNames;
-vector<string> variableValues;
-//The 2 above vectors should be thought of as parallel, 
 
 using namespace std;
+
+void execCommand(vector<string> tokens);
+vector<string> createTokens(string str);
+void changePrompt(vector<string> tokens);
+void setvar(vector<string> tokens);
+
+bool ShowTokens = false;
+string prompt = "msh > ";
+
+vector<string> variableNames;
+vector<string> variableValues;
+
+//The 2 above vectors should be thought of as parallel, 
 
 vector<string> createTokens(string str) {
     istringstream iss(str);
@@ -24,21 +33,26 @@ vector<string> createTokens(string str) {
     return tokens;
 }
 void execCommand(vector<string> tokens) {
-	string command = tokens[0];
-	if (strcmp(command, "setvar") == 0) {
-		setvar();
-	}
-	//keep creating else if's for rest of commands
+    string command = tokens[0];
+    if (command == "setvar") {
+        setvar(tokens);
+    } else if (command == "setprompt") {
+        changePrompt(tokens);
+    }
+    //keep creating else if's for rest of commands
+}
+
+void changePrompt(vector<string> tokens) {
+    prompt = tokens[1];
 }
 
 int main() {
-    string cmd;
     string command;
-	char *parameters[PARAMETERSMAX];
-	char *envp[] = { (char*) "PATH!=bin", 0 };
+    char *parameters[PARAMETERSMAX];
+    char *envp[] = { (char*) "PATH!=bin", 0 };
 
     while (true) {
-        cout << "Enter command: ";
+        cout << prompt;
         getline(cin, command);
         cout << "Command: \"" << command << "\"!\n";
         vector<string> tokens = createTokens(command);
