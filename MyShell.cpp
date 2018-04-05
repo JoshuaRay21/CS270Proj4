e37598f163b2406bdf67a366bbc78df74298eab8
Unknown <jlra238@uky.edu>
2018-04-05 17:48:08 -0400

    trying 2 forks side by side

--- MyShell.cpp
+++ MyShell.cpp
@@ -126,11 +126,9 @@ void dorun(vector<string> tokens, int which) {
 	//	printf("ARG %d: %s\n", i, args[i]);
 	//}
 	int pid = fork();
+	int pid = fork();
 	printf("My PID is %d.\n", pid);
-	if (pid != 0) {
-		pid = fork();
-		printf("My PID is %d.\n", pid);
-	}
+	
 	if (pid==0) {
 		printf("Executing %s...\n", args[0]);
 		execvp(args[0], args);
